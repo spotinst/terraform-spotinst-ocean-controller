a7185b1e975b776c2baa7bb3b7f887bdcb0ade76
liranp <liran.polak@gmail.com>
2020-03-30 15:11:26 +0300

    chore(release): upgrade controllver version to 1.0.58

--- main.tf
+++ main.tf
@@ -258,7 +258,7 @@ resource "kubernetes_deployment" "this" {
         }
 
         container {
-          image             = "spotinst/kubernetes-cluster-controller:1.0.57"
+          image             = "spotinst/kubernetes-cluster-controller:1.0.58"
           name              = "spotinst-kubernetes-cluster-controller"
           image_pull_policy = "Always"
 
