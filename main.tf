365f28bfe96fd435e4d59d184f28b4c3a666bf3f
Kyle Lindeque <git@arte.mu>
2021-01-13 12:02:33 +0000

    Add version variable for quicker updating of controller
    Docker image was hard coded, meaning that there was a delay in pushing updates to the versioning.
    This way, module users are able to push new versions without branching or waiting for an update. Kept version on the pinned tag rather than `latest` tag.

--- main.tf
+++ main.tf
@@ -333,7 +333,7 @@ resource "kubernetes_deployment" "this" {
         }
 
         container {
-          image             = "spotinst/kubernetes-cluster-controller:1.0.69"
+          image             = "spotinst/kubernetes-cluster-controller:${var.controller_version}"
           name              = "spotinst-kubernetes-cluster-controller"
           image_pull_policy = "Always"
 
