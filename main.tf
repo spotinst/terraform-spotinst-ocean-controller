678a8d325b7613503f0f69844cc9386c1ceb66e4
Sharad Kesarwani <108344822+sharadkesarwani@users.noreply.github.com>
2024-01-18 14:45:26 +0530

    created dependency of SA to create secret
    created dependency of SA to create secret

--- main.tf
+++ main.tf
@@ -39,19 +39,6 @@ resource "kubernetes_config_map" "this" {
     "disable-auto-update"         = var.disable_auto_update
   }
 }
-resource "kubernetes_secret" "sa_token" {
-  metadata {
-    name = "${local.service_account_name}-token"
-    namespace = local.namespace
-    annotations = {
-      "kubernetes.io/service-account.name" = local.service_account_name
-    }
-  }
-  type = "kubernetes.io/service-account-token"
-
-  depends_on = [kubernetes_service_account.this]
-}
-
 resource "kubernetes_service_account" "this" {
   count = var.create_controller ? 1 : 0
 
@@ -61,6 +48,20 @@ resource "kubernetes_service_account" "this" {
   }
   automount_service_account_token = false
 
+  secret {
+    name = "${local.service_account_name}-token"
+  }
+}
+
+resource "kubernetes_secret" "sa_token" {
+  metadata {
+    name = "${local.service_account_name}-token"
+    namespace = local.namespace
+    annotations = {
+      "kubernetes.io/service-account.name" = kubernetes_service_account.this[0].metadata[0].name
+    }
+  }
+  type = "kubernetes.io/service-account-token"
 }
 
 resource "kubernetes_cluster_role" "this" {
